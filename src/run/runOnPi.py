7a5d80e721ae15f862aeec80377ac888fe7f9edb
Noah Albers <albers-noah@pm.me>
2022-07-26 15:00:27 +0200

    Fixed bug with pong game positioning of a player

--- src/run/runOnPi.py
+++ src/run/runOnPi.py
@@ -4,5 +4,5 @@ from core.rendering.Screen import Screen
 from run.start import start
 from games.pong.Pong import Pong
 
-# TODO: Currently the WS2812B-Renderer is not existing, therefore the ANSI-Renderer is here
-start(ANSIRenderer(Screen(18, 20)), SerialEspUserInput(), Pong())
+# TODO: Currently the WS2812B-Renderer is not existing, therefor the ANSI-Renderer is here
+start(ANSIRenderer(Screen(18, 24)), SerialEspUserInput(), Pong())
