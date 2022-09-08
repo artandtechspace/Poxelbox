2663fa2f178e22f9327858605bb87caec84dbf62
mhefb <106405263+mhefb@users.noreply.github.com>
2022-09-08 17:31:29 +0200

    Made previews for Tetris and Snake in 15 px x 20 px Preview images are now only loaded if they match the screen size, 	indicated by the ending of the file name Arrows on preview images are now dynamicly generated from an image

--- src/scenes/LoadingScreenScene.py
+++ src/scenes/LoadingScreenScene.py
@@ -6,22 +6,23 @@ from core.rendering.renderer.RendererBase import RendererBase
 from PIL import Image
 import config.Config as Cfg
 from config import ControllerKeys as Controller
+from core.util.Vector2D import Vector2D
 from scenes.snake import SnakeScene
 import scenes.pong.PongScene as PongScene
 
-ARROWS = "rsc//previews//arrows.png"
+ARROW = "rsc//previews//arrow.png"
 ARROW_COLOR = Colors.WHITE
-PREVIEWS = ["rsc//previews//pong.png", "rsc//previews//snake.png"]
+PREVIEWS = ["rsc//previews//tetris", "rsc//previews//snake"]
 
 
 class LoadingScreenScene(SceneBase):
     images: []
-    arrows: Image
+    arrow_positions: [Vector2D[int], Vector2D[int]]
     scenes: []
     game_idx: int
 
     def get_time_constant(self):
-        return .1
+        return 1
 
     def on_init(self, scene_controller: SceneController, renderer: RendererBase, player_one: Player,
                 player_two: Player):
@@ -30,28 +31,17 @@ class LoadingScreenScene(SceneBase):
         self.scene_controller = scene_controller
         self.images = []
         self.scenes = [PongScene.PongScene(), SnakeScene.SnakeScene()]
-        self.arrows = Image.open(ARROWS)
+        self.arrow_positions = []
 
-        # Loads every image
-        for i in range(len(PREVIEWS)):
-            self.images.append(Image.open(PREVIEWS[i]))
-            # Raises Exception when the Image does not match the screen size
-            # if self.images[i].size != (renderer.screen.size_x, renderer.screen.size_y):
-            #     raise Exception("Wrong Image size!")
+        self.__generate_arrow_positions()
+        self.__load_images()
 
         # loads the image and sets the game index to start value
-        self.reload()
-
-    def __display_image(self, idx: int):
-        t_img = self.images[idx]
-        # iterates through every pixel and displays the pixels colour at its position
-        for x in range(t_img.size[0]):
-            for y in range(t_img.size[1]):
-                color = t_img.getpixel((x, y))[0:3]
-                if self.arrows.getpixel((x, y)):
-                    color = ARROW_COLOR
-                self.renderer.set_led(x, t_img.size[1] - y - 1, color)
-        self.renderer.push_leds()
+        self.game_idx = 0
+        if len(self.images) != 0:
+            self.__display_image(self.game_idx)
+        else:
+            print("Could not fine a preview image fitting for the screen")
 
     # no updates needed
     def on_update(self):
@@ -81,7 +71,37 @@ class LoadingScreenScene(SceneBase):
                     self.renderer.fill(0, 0, self.renderer.screen.size_x, self.renderer.screen.size_y, Colors.OFF)
                     self.scene_controller.load_scene(self.scenes[self.game_idx])
 
-    # loads the image and sets the game index to start value
-    def reload(self):
-        self.game_idx = 0
-        self.__display_image(self.game_idx)
+    # Generates the arrow overlay for the previews
+    def __generate_arrow_positions(self):
+        arrow = Image.open(ARROW)
+        # Iterates through every pixel of the arrow and maps its desired positions relative to the screen
+        for x in range(1, arrow.size[0] + 1):
+            for y in range(arrow.size[1]):
+                if arrow.getpixel((x - 1, y)):
+                    pos_y = self.renderer.screen.size_y + 1 - int(arrow.size[1] / 2) - y
+                    self.arrow_positions.append(Vector2D[int](x, pos_y))
+                    self.arrow_positions.append(Vector2D[int](self.renderer.screen.size_x - x - 1, pos_y))
+
+    def __display_image(self, idx: int):
+        t_img = self.images[idx]
+        # iterates through every pixel and displays the pixels colour at its position
+        for x in range(t_img.size[0]):
+            for y in range(t_img.size[1]):
+                color = t_img.getpixel((x, y))[0:3]
+                if Vector2D[int](x, y) in self.arrow_positions:
+                    color = ARROW_COLOR
+                self.renderer.set_led(x, t_img.size[1] - y - 1, color)
+        self.renderer.push_leds()
+
+    # Loads images
+    def __load_images(self):
+        for preview in PREVIEWS:
+            # searches for the preview with equal size
+            preview += str(self.renderer.screen.size_x)
+            preview += "x"
+            preview += str(self.renderer.screen.size_y)
+            preview += ".png"
+            try:
+                self.images.append(Image.open(preview))
+            except:
+                pass
