d569825223883ea3bff9661656b535bbeace626c
mhefb <106405263+mhefb@users.noreply.github.com>
2025-08-15 18:09:14 +0200

    renamed config parameters; added clamp helperfunction; optimized set_color_brightness helperfunction; renamed start_fade_in_until_nex_flush; implemented fade playback in its own function

--- src/core/rendering/renderer/BrightnessControlledAndOptionalFadeInRenderer.py
+++ src/core/rendering/renderer/BrightnessControlledAndOptionalFadeInRenderer.py
@@ -3,19 +3,26 @@ import config.Config as Cfg
 import time
 
 class configuration:
-    FADE_IN_FRAMES: int = 30
-    FADE_IN_DURATION: float = 1 # in seconds
-    pass
+    RENDERER_FADE_IN_FRAMES: int = 30
+    RENDERER_FADE_IN_DURATION: float = 1 # in seconds
+    # in range [0, 1] where 0 ~ black; 1 ~ unchanged color
+    RENDERER_BRIGHTNESS_MAXVALUE: float = 1.0
 
 #region Helperfunctions
 
 @staticmethod
 def lerp( color_a: (int, int, int), color_b: (int, int, int), t: float ):
-    return tuple( a * (1-t) + b * t for a, b in zip(color_a, color_b) )
+    return tuple( int(a * (1-t) + b * t) for a, b in zip(color_a, color_b) )
 
 @staticmethod
 def set_color_brightness(color: (int, int, int), brightness: float):
-    return lerp( (0, 0, 0), color, brightness )
+    # conceptually the same as: 
+    # return lerp( (0, 0, 0), color, brightness )
+    return tuple( int(b * t) for b in color )
+
+@staticmethod
+def clamp(lower_bound, upper_bound, value):
+    return min( lower_bound, max(upper_bound, value))
 
 #endregion
 
@@ -30,29 +37,38 @@ class BrightnessControlledAndOptionalFadeInRenderer(RendererBase):
     def setup(self):
         super().setup()
 
-    def start_fade_in_until_nex_flush(self):
+    def start_capture_for_fade_in(self):
         self.caputured_set_led_calls = []
         self.is_capturing_screen = True
 
     def set_led(self, x: int, y: int, color: (int, int, int)):
+        """
+        in child overrides:
+        color = super(x, y, color) # brightess adjusted color
+        if not color: # fade in abort
+            return
+        """
         if self.is_capturing_screen:
+            # NOTE does override old capture when set_led is called on the same coordinate
             self.caputured_set_led_calls.append((x, y, color))
             return False
-        return True
-
-    def __set_brightness(self, brightness : float, color: (int, int, int)):
-        # brightness = min(0, max(1, brightness))
-        pass
+        return set_color_brightness(color=color, brightness=configuration.RENDERER_BRIGHTNESS_MAXVALUE)
 
     def push_leds(self):
-        sleep_duration = configuration.FADE_IN_DURATION / configuration.FADE_IN_FRAMES
-        for i in range(configuration.FADE_IN_FRAMES):
+        pass
+    
+    def play_fade_in(self):
+        if not (self.is_capturing_screen and self.caputured_set_led_calls):
+            return
+        self.is_capturing_screen = False
+        sleep_duration = configuration.RENDERER_FADE_IN_DURATION / configuration.RENDERER_FADE_IN_FRAMES
+        for i in range(configuration.RENDERER_FADE_IN_FRAMES):
             for caputured_call in self.caputured_set_led_calls:
                 self.set_led(
                         caputured_call[0],
                         caputured_call[1],
-                        set_color_brightness(caputured_call[2], i/configuration.FADE_IN_FRAMES)
+                        set_color_brightness(caputured_call[2], i/configuration.RENDERER_FADE_IN_FRAMES)
                 )
+            self.push_leds()
             time.sleep(sleep_duration)
         self.caputured_set_led_calls = []
-        self.is_capturing_screen = False
